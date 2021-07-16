ee10c094cf9a919942b281653c83fcca79aae6e1
Ashraf ali <examsign.ashraf@outlook.com>
2021-07-16 20:43:51 +0530

    Day Converter

---
+++ MicroFinance/Converters/DayConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MicroFinance.Converters
+{
+    class DayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime Date = (DateTime)value;
+            return Date.DayOfWeek;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
