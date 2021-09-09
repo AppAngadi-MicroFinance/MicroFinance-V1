be92f519a0e8a449378bfbf5bba8c17a89ff3b9f
ExamSignAshraf <examsign.ashraf@outlook.com>
2021-09-09 11:51:12 +0530

    Collection Report Sownload For Single Center

--- MicroFinance/DownloadCollectionReport.xaml.cs
+++ MicroFinance/DownloadCollectionReport.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MicroFinance.ViewModel;
+using Microsoft.Reporting.WinForms;
 
 namespace MicroFinance
 {
@@ -53,11 +57,146 @@ namespace MicroFinance
             }
 
         }
+        void LoadCenter(string FOID)
+        {
+            CenterNameCombo.Items.Clear();
+            CenterNameView Allcenter = new CenterNameView();
+            Allcenter.CenterId = "All";
+            Allcenter.CenterName = "All";
+            Allcenter.BranchId = "All";
+            CenterNameCombo.Items.Add(Allcenter);
+            foreach(CenterNameView C in CenterList)
+            {
+                if(C.EmpId==FOID)
+                {
+                    CenterNameCombo.Items.Add(C);
+                }
+            }
+        }
 
         private void BranchNameCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            FONameCombo.IsEnabled = true;
             BranchNameView SelectedBranch = BranchNameCombo.SelectedValue as BranchNameView;
             LoadFO(SelectedBranch.BranchId);
+        }
+
+        private void FONameCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CenterNameCombo.IsEnabled = true;
+            EmployeeNameView SelectedEmployee = FONameCombo.SelectedValue as EmployeeNameView;
+            LoadCenter(SelectedEmployee.EmpId);
+        }
+
+        private async void DownloadSheetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(BranchNameCombo.SelectedValue!=null && FONameCombo.SelectedValue!=null && CenterNameCombo.SelectedValue!=null)
+            {
+                BranchNameView SelectedBranch = BranchNameCombo.SelectedValue as BranchNameView;
+                EmployeeNameView SelectedEmployee = FONameCombo.SelectedValue as EmployeeNameView;
+                CenterNameView selectedCenter = CenterNameCombo.SelectedValue as CenterNameView;
+                GifPanel.Visibility = Visibility.Visible;
+                string branchId = SelectedBranch.BranchId;
+                string CenterID = selectedCenter.CenterId;
+                string EmpId = SelectedEmployee.EmpId;
+                DateTime selectedDate = CollectionDate.SelectedDate.Value;
+
+                await System.Threading.Tasks.Task.Run(()=> GenerateReport(branchId,CenterID,EmpId,selectedDate));
+                GifPanel.Visibility = Visibility.Collapsed;
+                MainWindow.StatusMessageofPage(1, "Report generated Successfully!....");
+            }
+        }
+        void GenerateReport(string BranchID,string CenterId,String EmpID,DateTime date)
+        {
+            
+            //DateTime selecteddate = CollectionDate.SelectedDate.Value;
+            List<CollectionReportView> CollectionList = CollectionReportRepo.GetCollectionDetails(BranchID,EmpID,CenterId,date);
+            GenerateCollectionReport(CollectionList);
+        }
+
+        static public DataTable ConvertToDataTable<T>(IList<T> data)
+        {
+            PropertyDescriptorCollection properties =
+               TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+            return table;
+
+        }
+
+
+        static void GenerateCollectionReport(List<CollectionReportView> obj)
+        {
+            //string filedate = showDate.Text;
+            DataTable dt1 = new DataTable();
+            dt1 = ConvertToDataTable(obj);
+
+            
+            //DataTable dt2 = new DataTable();
+            //dt2 = ConvertToDataTable(obj);
+
+
+            //For table 1
+            ReportDataSource reportDataSource1 = new ReportDataSource();
+            reportDataSource1.Name = "DataSet1"; // Name of the DataSet we set in .rdlc
+            reportDataSource1.Value = dt1;
+
+            //For table 2
+            //ReportDataSource reportDataSource2 = new ReportDataSource();
+            //reportDataSource2.Name = "DataSet2"; // Name of the DataSet we set in .rdlc
+            //reportDataSource2.Value = dt2;
+
+            //Setting Report Viewer
+            ReportViewer reportViewer1 = new ReportViewer();
+            reportViewer1.LocalReport.ReportEmbeddedResource = "MicroFinance.CollectionReport.rdlc";
+            reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            //reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            reportViewer1.RefreshReport();
+            reportViewer1.ProcessingMode = ProcessingMode.Local;
+
+            Warning[] warnings1;
+            string[] streamids1;
+            string mimeType1;
+            string encoding1;
+            string extension1;
+            try
+            {
+                string dir = string.Empty;
+                //string showdatess = Changeformat(showDate.Text);
+                byte[] bytes = reportViewer1.LocalReport.Render("PDF", null, out mimeType1, out encoding1, out extension1, out streamids1, out warnings1);
+                dir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "REPORTS\\");
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                if (Directory.Exists(dir))
+                {
+                    //var temp = "" + data + "" + "_WeeklyReport"+showDate+"";
+                    //var temp = "" + data + "" + "_" + "" + SelectedCluster + "";
+                    FileStream fs = new FileStream(dir + "Collection_Report_Sheet_" + DateTime.Now.ToString("dd-MMM-yyyy") + ".pdf", FileMode.Create);
+
+                    var temps = fs.ToString();
+                    fs.Write(bytes, 0, bytes.Length);
+                    fs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can't generate Collection Sheet for You. Contact admin.");
+            }
+        }
+
+        private void CenterNameCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CollectionDate.IsEnabled = true;
         }
     }
 }
--- MicroFinance/ViewModel/CollectionReportRepo.cs
+++ MicroFinance/ViewModel/CollectionReportRepo.cs
@@ -12,50 +12,50 @@ namespace MicroFinance.ViewModel
     {
 
 
-        public static List<CollectionReportView> GetCollectionDetails(string branchID,string GroupID,DateTime CollectionDate)
-        {
-            List<CollectionReportView> CollectionDetialsList = new List<CollectionReportView>();
-            List<CollectionDetails> Details = new List<CollectionDetails>();
-            using(SqlConnection sqlconn=new SqlConnection(MicroFinance.Properties.Settings.Default.DBConnection))
-            {
-                sqlconn.Open();
-                if(ConnectionState.Open==sqlconn.State)
-                {
-                    SqlCommand sqlcomm = new SqlCommand();
-                    sqlcomm.Connection = sqlconn;
-                    sqlcomm.CommandText = "select CustomerGroup.CustId,LoanCollectionEntry.BranchId,LoanCollectionEntry.Total,LoanCollectionEntry.CollectedBy from LoanCollectionEntry,CustomerGroup where CollectedOn='"+CollectionDate+"' and LoanCollectionEntry.CustId=CustomerGroup.CustId and CustomerGroup.PeerGroupId in(select GroupId from PeerGroup where SHGid='" + GroupID + "')and LoanCollectionEntry.BranchId='"+branchID+"'";
-                    SqlDataReader reader = sqlcomm.ExecuteReader();
-                    if(reader.HasRows)
-                    {
-                        while(reader.Read())
-                        {
-                            Details.Add(new CollectionDetails { CustID = reader.GetString(0), BranchId = reader.GetString(1), Total = reader.GetInt32(2), EmpId = reader.GetString(3) });
-                        }
-                    }
-                    reader.Close();
+        //public static List<CollectionReportView> GetCollectionDetails(string branchID,string GroupID,DateTime CollectionDate)
+        //{
+        //    List<CollectionReportView> CollectionDetialsList = new List<CollectionReportView>();
+        //    List<CollectionDetails> Details = new List<CollectionDetails>();
+        //    using(SqlConnection sqlconn=new SqlConnection(MicroFinance.Properties.Settings.Default.DBConnection))
+        //    {
+        //        sqlconn.Open();
+        //        if(ConnectionState.Open==sqlconn.State)
+        //        {
+        //            SqlCommand sqlcomm = new SqlCommand();
+        //            sqlcomm.Connection = sqlconn;
+        //            sqlcomm.CommandText = "select CustomerGroup.CustId,LoanCollectionEntry.BranchId,LoanCollectionEntry.Total,LoanCollectionEntry.CollectedBy from LoanCollectionEntry,CustomerGroup where CollectedOn='"+CollectionDate+"' and LoanCollectionEntry.CustId=CustomerGroup.CustId and CustomerGroup.PeerGroupId in(select GroupId from PeerGroup where SHGid='" + GroupID + "')and LoanCollectionEntry.BranchId='"+branchID+"'";
+        //            SqlDataReader reader = sqlcomm.ExecuteReader();
+        //            if(reader.HasRows)
+        //            {
+        //                while(reader.Read())
+        //                {
+        //                    Details.Add(new CollectionDetails { CustID = reader.GetString(0), BranchId = reader.GetString(1), Total = reader.GetInt32(2), EmpId = reader.GetString(3) });
+        //                }
+        //            }
+        //            reader.Close();
                     
-                    foreach(CollectionDetails CD in Details)
-                    {
-                        CollectionReportView CollectionData = new CollectionReportView();
-                        sqlcomm.CommandText = "select BranchName from BranchDetails where Bid='" + CD.BranchId + "'";
-                        CollectionData.BranchName = (string)sqlcomm.ExecuteScalar();
-                        sqlcomm.CommandText = "select Name from Employee where EmpId = '"+CD.EmpId+"'";
-                        CollectionData.FoName = (string)sqlcomm.ExecuteScalar();
-                        sqlcomm.CommandText = "select Name from CustomerDetails where CustId='" + CD.CustID + "'";
-                        CollectionData.CustomerName = (string)sqlcomm.ExecuteScalar();
-                        CollectionData.Amount = CD.Total;
-                        sqlcomm.CommandText = "select SHGName from SelfHelpGroup where SHGId=(select SHGid from PeerGroup where GroupId=(select peerGroupId from CustomerGroup where CustId='"+CD.CustID+"'))";
-                        CollectionData.CenterName = (string)sqlcomm.ExecuteScalar();
+        //            foreach(CollectionDetails CD in Details)
+        //            {
+        //                CollectionReportView CollectionData = new CollectionReportView();
+        //                sqlcomm.CommandText = "select BranchName from BranchDetails where Bid='" + CD.BranchId + "'";
+        //                CollectionData.BranchName = (string)sqlcomm.ExecuteScalar();
+        //                sqlcomm.CommandText = "select Name from Employee where EmpId = '"+CD.EmpId+"'";
+        //                CollectionData.FoName = (string)sqlcomm.ExecuteScalar();
+        //                sqlcomm.CommandText = "select Name from CustomerDetails where CustId='" + CD.CustID + "'";
+        //                CollectionData.CustomerName = (string)sqlcomm.ExecuteScalar();
+        //                CollectionData.Amount = CD.Total;
+        //                sqlcomm.CommandText = "select SHGName from SelfHelpGroup where SHGId=(select SHGid from PeerGroup where GroupId=(select peerGroupId from CustomerGroup where CustId='"+CD.CustID+"'))";
+        //                CollectionData.CenterName = (string)sqlcomm.ExecuteScalar();
 
-                        CollectionDetialsList.Add(CollectionData);
-                    }
-                    sqlconn.Close();
-                }
-                return CollectionDetialsList;
-            }
+        //                CollectionDetialsList.Add(CollectionData);
+        //            }
+        //            sqlconn.Close();
+        //        }
+        //        return CollectionDetialsList;
+        //    }
 
 
-        }
+        //}
 
         public static List<BranchNameView> GetBranchNames()
         {
@@ -94,13 +94,13 @@ namespace MicroFinance.ViewModel
                 {
                     SqlCommand sqlcomm = new SqlCommand();
                     sqlcomm.Connection = sqlconn;
-                    sqlcomm.CommandText = "select BranchId,SHGId,SHGName from SelfHelpGroup";
+                    sqlcomm.CommandText = "select SelfHelpGroup.BranchId,SelfHelpGroup.SHGId,selfHelpGroup.SHGName,TimeTable.EmpId from SelfHelpGroup,TimeTable where SelfHelpGroup.SHGId=TimeTable.SHGId";
                     SqlDataReader reader = sqlcomm.ExecuteReader();
                     if(reader.HasRows)
                     {
                         while(reader.Read())
                         {
-                            CenterList.Add(new CenterNameView { BranchId = reader.GetString(0), CenterId = reader.GetString(1), CenterName = reader.GetString(2) });
+                            CenterList.Add(new CenterNameView { BranchId = reader.GetString(0), CenterId = reader.GetString(1), CenterName = reader.GetString(2),EmpId=reader.GetString(3) });
                         }
                         reader.Close();
                         sqlconn.Close();
@@ -136,6 +136,81 @@ namespace MicroFinance.ViewModel
             }
             return EmployeeList;
         }
+
+
+        public static List<CollectionReportView> GetCollectionDetails(string BranchId,string EmpId,string CenterID,DateTime CollectionDate)
+        {
+            List<CollectionReportView> CollectionList = new List<CollectionReportView>();
+            using(SqlConnection sqlconn=new SqlConnection(MicroFinance.Properties.Settings.Default.DBConnection))
+            {
+                sqlconn.Open();
+                if(ConnectionState.Open==sqlconn.State)
+                {
+                    SqlCommand sqlcomm = new SqlCommand();
+                    sqlcomm.Connection = sqlconn;
+                    sqlcomm.CommandText = "select BranchDetails.BranchName,SelfHelpGroup.SHGName,Employee.Name,CustomerDetails.Name,LoanCollectionEntry.Total from CustomerDetails,Employee,BranchDetails,SelfHelpGroup,LoanCollectionEntry where BranchDetails.Bid='"+BranchId+"' and SelfHelpGroup.SHGId='"+CenterID+"' and Employee.EmpId='"+EmpId+"' and CustomerDetails.CustId=LoanCollectionEntry.CustId and LoanCollectionEntry.CollectedBy='"+EmpId+"' and CollectedOn='"+CollectionDate+"'";
+                    SqlDataReader reader = sqlcomm.ExecuteReader();
+                    if(reader.HasRows)
+                    {
+                        while(reader.Read())
+                        {
+                            CollectionList.Add(
+                                new CollectionReportView
+                                {
+                                    BranchName=reader.GetString(0),
+                                    CenterName=reader.GetString(1),
+                                    FoName=reader.GetString(2),
+                                    CustomerName=reader.GetString(3),
+                                    Amount=reader.GetInt32(4),
+                                }
+                                );
+                        }
+                        reader.Close();
+                    }
+                    sqlconn.Close();
+                }
+            }
+            return CollectionList;
+        }
+
+
+
+        public static List<CollectionReportView> GetCollectionDetails(string BranchId, string EmpId,DateTime CollectionDate)
+        {
+            DayOfWeek day = CollectionDate.DayOfWeek;
+            List<CollectionReportView> CollectionList = new List<CollectionReportView>();
+            using (SqlConnection sqlconn = new SqlConnection(MicroFinance.Properties.Settings.Default.DBConnection))
+            {
+                sqlconn.Open();
+                if (ConnectionState.Open == sqlconn.State)
+                {
+                    SqlCommand sqlcomm = new SqlCommand();
+                    sqlcomm.Connection = sqlconn;
+                    sqlcomm.CommandText = "select BranchDetails.BranchName,SelfHelpGroup.SHGName,Employee.Name,CustomerDetails.Name,LoanCollectionEntry.Total from CustomerDetails,Employee,BranchDetails,SelfHelpGroup,LoanCollectionEntry where BranchDetails.Bid='01202109001' and SelfHelpGroup.SHGId='01001202109SHG-18' and Employee.EmpId='"+EmpId+"' and CustomerDetails.CustId=LoanCollectionEntry.CustId and LoanCollectionEntry.CollectedBy='E0100120210903' and CollectedOn='"+CollectionDate+"' and SelfHelpGroup.SHGId in (select SHGId from TimeTable where EmpId='"+EmpId+"' and CollectionDay='"+day+"')";
+                    SqlDataReader reader = sqlcomm.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            CollectionList.Add(
+                                new CollectionReportView
+                                {
+                                    BranchName = reader.GetString(0),
+                                    CenterName = reader.GetString(1),
+                                    FoName = reader.GetString(2),
+                                    CustomerName = reader.GetString(3),
+                                    Amount = reader.GetInt32(4),
+                                }
+                                );
+                        }
+                        reader.Close();
+                    }
+                    sqlconn.Close();
+                }
+            }
+            return CollectionList;
+        }
+
     }
     public class BranchNameView
     {
@@ -147,6 +222,7 @@ namespace MicroFinance.ViewModel
         public string CenterName { get; set; }
         public string CenterId { get; set; }
         public string BranchId { get; set; }
+        public string EmpId { get; set; }
     }
     public class EmployeeNameView
     {
