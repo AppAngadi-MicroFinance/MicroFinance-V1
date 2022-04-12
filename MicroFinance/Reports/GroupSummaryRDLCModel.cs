2d2c2ce4868933bbc82806fba815d3cc9b839dac
thalif <Examsign.thalif@ourlook.com>
2022-04-12 17:13:22 +0530

    Inegeration successfull

---
+++ MicroFinance/Reports/GroupSummaryRDLCModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroFinance.Reports
+{
+    public class GroupSummaryRDLCModel
+    {
+        public string GroupName { get; set; }
+        public int CollectionAmount { get; set; }
+        public GroupSummaryRDLCModel()
+        {
+
+        }
+        public GroupSummaryRDLCModel(string groupName, int collectionAmount)
+        {
+            this.GroupName = groupName;
+            this.CollectionAmount = collectionAmount;
+        }
+    }
+}
